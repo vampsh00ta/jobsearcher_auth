01b6cbda73d80048bd164ab206e7a3d0e74db84c
vampsh00ta <trofimov.vlad-1234@yandex.ru>
2025-01-21 16:23:42 +0300

    very very minor fixes

--- internal/grpc/auth.go
+++ internal/grpc/auth.go
@@ -6,10 +6,26 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+	isrvc "jobsearcher_auth/internal/app/service"
 	"jobsearcher_auth/internal/entity"
 	"jobsearcher_auth/internal/pb"
 )
 
+type authGRPC struct {
+	pb.UnimplementedAuthServer
+
+	l        *zap.Logger
+	authSrvc isrvc.Auth
+	linkSrvc isrvc.Link
+}
+
+func New(authSrvc isrvc.Auth, linkSrvc isrvc.Link, l *zap.Logger) *authGRPC {
+	return &authGRPC{
+		authSrvc: authSrvc,
+		linkSrvc: linkSrvc,
+		l:        l,
+	}
+}
 func Register(s *grpc.Server, auth *authGRPC) {
 	pb.RegisterAuthServer(s, auth)
 }
